e02bed1ab8c857c30b20dfd5b5f8a24e91ab6756
Joker746U <amlan1922006@gmail.com>
2026-09-17 10:30:30 +0530

    Fix script structure and remove unnecessary code

--- Q1.sh
+++ Q1.sh
@@ -17,23 +17,4 @@ then
     echo "Palindrome"
 else
     echo "Not Palindrome"
-fi#!/bin/bash
-
-echo "Enter string: "
-read str
-
-rev=""
-i=0
-
-while [ $i -lt ${#str} ]
-do
-    rev="${str:$i:1}$rev"
-    i=$((i + 1))
-done
-
-if [ "$str" = "$rev" ]
-then
-    echo "Palindrome"
-else
-    echo "Not Palindrome"
 fi
