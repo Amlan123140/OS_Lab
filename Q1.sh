9a838735198047b388701925044c173c15582db2
Joker746U <amlan1922006@gmail.com>
2026-09-17 10:13:10 +0530

    Refactor Q1.sh to check for string palindrome
    Removed number palindrome check and added string palindrome check.

--- Q1.sh
+++ Q1.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-echo "Enter x: "
-read x
-Str=$x
-rev=0
-while [ $x -gt 0 ]
-do	
-	n=$((x % 10))
-	rev=$((10 * rev + n))
-	x=$((x / 10))
+
+echo "Enter string: "
+read str
+
+i=0
+j=$((${#str} - 1))
+
+while [ $i -lt $j ]
+do
+    if [ "${str:$i:1}" != "${str:$j:1}" ]
+    then
+        echo "Not Palindrome"
+        exit
+    fi
+
+    i=$((i + 1))
+    j=$((j - 1))
 done
 
-if [ $Str -eq $rev ]
-then
-	echo "Pallindrome"
-else
-	echo "Not Pallindrome"
-fi
-echo "Question Solved! Great job!."
+echo "Palindrome"
